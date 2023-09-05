98e1bfa371d5155598b8ed2de9caebeaa9f8fa0b
Sihwan Yu <tlghks1217@naver.com>
2023-09-05 15:24:57 +0900

    feat: #18 아키텍처 변경 및 서킷브레이커 적용

--- src/main/java/com/example/auctionserver/adapter/out/feign/MemberServerClientFallback.java
+++ src/main/java/com/example/auctionserver/adapter/out/feign/MemberServerClientFallback.java
@@ -14,12 +14,12 @@ public class MemberServerClientFallback implements MemberServiceClient {
     public ResponseEntity<ResponsePointDto> getPoint(Long memberId) {
 
         log.info("포인트 조회 실패 -> Fallback");
-        throw new IllegalStateException("회원 정보를 불러올 수 없습니다");
+        throw new IllegalStateException("멤버 서버로부터 정보를 불러올 수 없습니다");
     }
 
     @Override
     public void validatePoint(Long bidPoint, Long memberId) {
         log.info("포인트 검증 실패 -> Fallback");
-        throw new IllegalArgumentException("유효하지 않은 회원 포인트 정보입니다");
+        throw new IllegalArgumentException("유효하지 않은 멤버 포인트 정보입니다");
     }
 }
--- src/main/java/com/example/auctionserver/global/config/Resilience4jConfig.java
+++ src/main/java/com/example/auctionserver/global/config/Resilience4jConfig.java
@@ -33,4 +33,6 @@ public class Resilience4jConfig {
                         .build()
         );
     }
+
+
 }
